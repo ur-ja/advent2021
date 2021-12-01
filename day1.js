5820b800ab78a0fd187bbb3520fa189fb5e7bb97
Urja Arora <urjaarora5@gmail.com>
2021-12-01 23:02:28 +0530

    Day01 Part01

---
+++ day1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const array = fs
+  .readFileSync("day1.txt", { encoding: "utf-8" })
+  .split("\n")
+  .filter((x) => x)
+  .map((x) => parseInt(x));
+
+let i = 0;
+let num_of_increase = 0;
+
+for (i; i < array.length; i++) {
+  if (array[i] < array[i + 1]) {
+    num_of_increase += 1;
+  }
+}
+
+console.log(num_of_increase);
